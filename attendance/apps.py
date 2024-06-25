9155e42595f3f10a6d8a43defeb2a31f808515a5
MielAndMoon <dmestasa@gmail.com>
2024-06-24 23:00:22 -0500

    feat(attendance): set custom admin site

--- attendance/apps.py
+++ attendance/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
+class AttendanceAdminConfig(AppConfig):
+    default_site = 'attendance.admin.AttendanceAdminArea'
+
+
 class AttendanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'attendance'
